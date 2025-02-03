e798e4720d417755e126026697e645da0d3e96ef
VaricelaZoster <maildwaipayansaha@gmail.com>
2025-02-04 00:39:14 +0530

    Giphy api added gifs in home page

--- giphyClone/src/Pages/Home.jsx
+++ giphyClone/src/Pages/Home.jsx
@@ -1,8 +1,35 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { GifState } from '../context/context'
+import Gif from '../Components/gif'
 
 const Home = () => {
+  const {gf,gifs,setGifs,filter} = GifState()
+  const fetchTrendingGifs = async () => {
+    const {data} = await gf.trending({
+      limit:20,
+      type: filter,
+      rating: "pg-13",
+    })
+    setGifs(data)
+  }
+
+  useEffect(() => {
+    fetchTrendingGifs()
+  },[filter])
+
   return (
-    <div>Home</div>
+    <div>
+      <img src="/banner.gif"
+            alt="earth banner"
+            className='mt-2 rounded w-full'>
+      </img>
+      {/*<FilterGif/>*/}
+      <div className='columns-2 md:columns-3 lg:columns-4 xl:columns-5 gap-2'>
+        {gifs.map((gif) => {
+          return(<Gif gif={gif} key={gif.title}/>)
+        })}
+      </div>
+    </div>
   )
 }
 
