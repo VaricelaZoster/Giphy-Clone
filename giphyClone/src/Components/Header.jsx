d664a3fa13a830ca0aed7a7ac48772be97163f7a
VaricelaZoster <maildwaipayansaha@gmail.com>
2025-02-03 22:28:25 +0530

    Giphy api added for categories

--- giphyClone/src/Components/Header.jsx
+++ giphyClone/src/Components/Header.jsx
@@ -36,21 +36,25 @@ const Header = () => {
                     <HiEllipsisVertical size={35} className={`py-0.5 hover:gradient1 
                         ${showcategories? "gradient1" : ""} border-b-4 hidden lg:block`}/>
                 </button>
-                <div className="h-9 bg-gray-700 pt-1.5 px-6 cursor-pointer rounded">
+                {favorites.length>0 && (<div className="h-9 bg-gray-700 pt-1.5 px-6 cursor-pointer rounded">
                     <Link to="/favourites">Favourite GIFS</Link>
-                </div>
+                </div>)}
                 <button>
                     <HiMiniBars3BottomRight size={30} className="text-sky-400 block lg:hidden"/>
                 </button>
                 </div>
             {showcategories && (
             <div className="absolute right-0 top-14 px-10 pt-6 pb-9 w-full gradient1 z-20">
-                <span>Categories</span>
-                <hr/>
-                <div>
-                    <Link className="font-bold">
-                        Reactions
-                    </Link>
+                <span className="text-3xl font-extrabold">Categories</span>
+                <hr className="bg-gray-100 opacity-50 my-5"/>
+                <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-rows-4 lg:grid-cols-5 xl:grid-cols-6 gap-4">
+                    {categories?.map((category) => {
+                        return(<Link className="font-bold"
+                                key={category.name}
+                                to={`/${category.name_encoded}`}>
+                        {category.name}
+                    </Link>)
+                    })}
                 </div>
             </div>)}
             </div>
